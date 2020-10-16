892f00caccbd9bddac3651462628373103eeadfe
Jaylan Lee <jaylan2993@gmail.com>
2020-10-16 18:58:42 +0200

    idk what to put here

---
+++ REEEE/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace REEEE {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.playerTurn = new System.Windows.Forms.Label();
+            this.new_game = new System.Windows.Forms.Button();
+            this.help_button = new System.Windows.Forms.Button();
+            this.blue_discs = new System.Windows.Forms.Label();
+            this.red_discs = new System.Windows.Forms.Label();
+            this.num_blue = new System.Windows.Forms.Label();
+            this.num_red = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(254, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(481, 481);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // playerTurn
+            // 
+            this.playerTurn.AutoSize = true;
+            this.playerTurn.Location = new System.Drawing.Point(49, 42);
+            this.playerTurn.Name = "playerTurn";
+            this.playerTurn.Size = new System.Drawing.Size(68, 17);
+            this.playerTurn.TabIndex = 1;
+            this.playerTurn.Text = "Start spel";
+            // 
+            // new_game
+            // 
+            this.new_game.Location = new System.Drawing.Point(32, 92);
+            this.new_game.Name = "new_game";
+            this.new_game.Size = new System.Drawing.Size(105, 29);
+            this.new_game.TabIndex = 2;
+            this.new_game.Text = "Nieuw Spel";
+            this.new_game.UseVisualStyleBackColor = true;
+            this.new_game.Click += new System.EventHandler(this.new_game_Click);
+            // 
+            // help_button
+            // 
+            this.help_button.Location = new System.Drawing.Point(32, 148);
+            this.help_button.Name = "help_button";
+            this.help_button.Size = new System.Drawing.Size(105, 29);
+            this.help_button.TabIndex = 3;
+            this.help_button.Text = "Help";
+            this.help_button.UseVisualStyleBackColor = true;
+            this.help_button.Click += new System.EventHandler(this.help_button_Click);
+            // 
+            // blue_discs
+            // 
+            this.blue_discs.AutoSize = true;
+            this.blue_discs.Location = new System.Drawing.Point(32, 231);
+            this.blue_discs.Name = "blue_discs";
+            this.blue_discs.Size = new System.Drawing.Size(96, 17);
+            this.blue_discs.TabIndex = 4;
+            this.blue_discs.Text = "Blauw stenen:";
+            // 
+            // red_discs
+            // 
+            this.red_discs.AutoSize = true;
+            this.red_discs.Location = new System.Drawing.Point(32, 271);
+            this.red_discs.Name = "red_discs";
+            this.red_discs.Size = new System.Drawing.Size(93, 17);
+            this.red_discs.TabIndex = 5;
+            this.red_discs.Text = "Rood stenen:";
+            // 
+            // num_blue
+            // 
+            this.num_blue.AutoSize = true;
+            this.num_blue.Location = new System.Drawing.Point(145, 231);
+            this.num_blue.Name = "num_blue";
+            this.num_blue.Size = new System.Drawing.Size(16, 17);
+            this.num_blue.TabIndex = 6;
+            this.num_blue.Text = "0";
+            // 
+            // num_red
+            // 
+            this.num_red.AutoSize = true;
+            this.num_red.Location = new System.Drawing.Point(145, 271);
+            this.num_red.Name = "num_red";
+            this.num_red.Size = new System.Drawing.Size(16, 17);
+            this.num_red.TabIndex = 7;
+            this.num_red.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 505);
+            this.Controls.Add(this.num_red);
+            this.Controls.Add(this.num_blue);
+            this.Controls.Add(this.red_discs);
+            this.Controls.Add(this.blue_discs);
+            this.Controls.Add(this.help_button);
+            this.Controls.Add(this.new_game);
+            this.Controls.Add(this.playerTurn);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label playerTurn;
+        private System.Windows.Forms.Button new_game;
+        private System.Windows.Forms.Button help_button;
+        private System.Windows.Forms.Label blue_discs;
+        private System.Windows.Forms.Label red_discs;
+        private System.Windows.Forms.Label num_blue;
+        private System.Windows.Forms.Label num_red;
+    }
+}
+
---
+++ REEEE/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace REEEE {
+    public partial class Form1 : Form {
+        static int[] num_to_bits = new int[16] 
+        { 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4 };
+        const int w = 60; //width of a cell
+        const int m = 3; // m for margin
+        ulong red;
+        ulong blue;
+        ulong board;
+        bool hints = false;
+        bool blueTurn;
+
+        public Form1() {
+            InitializeComponent();
+
+            initBoard();
+        }
+
+        void initBoard() {
+            blueTurn = true;
+            board = 0;
+            red = 0;
+            red |= 1L << (3 * 8 + 3);
+            red |= 1L << (4 * 8 + 4);
+            blue = 0;
+            blue |= 1L << (3 * 8 + 4);
+            blue |= 1L << (4 * 8 + 3);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e) {
+            int blueStones, redStones;
+            e.Graphics.FillRectangle(Brushes.LightGray, 0, 0, w*8, w*8);
+
+            blueStones = countSetBitsRec(blue);
+            redStones = countSetBitsRec(red);
+
+            //init possible moves and set label
+            initTurn();
+
+            drawRaster(e.Graphics);
+
+            //if no possible moves, yield turn to other player
+            if (board == 0L) {
+                if (~(red | blue) == 0L) {
+                    if(blueStones > redStones)
+                        playerTurn.Text = "Game over: Blauw wint";
+                    else if(blueStones != redStones)
+                        playerTurn.Text = "Game over: Rood wint";
+                    else
+                        playerTurn.Text = "Game over: Gelijkspel";
+                } else {
+                    blueTurn = !blueTurn;
+                    initTurn();
+                }
+            }
+
+            num_blue.Text = "" + countSetBitsRec(blue);
+            num_red.Text = "" + countSetBitsRec(red);
+
+            for (int j = 0; j < 8; j++) {
+                for (int i = 0; i < 8; i++) {
+                    ulong bit = (ulong)1L << (j * 8 + i);
+                    if ((red & bit) != 0)
+                        e.Graphics.FillEllipse(Brushes.Red, i * w + m, j * w + m, w - 2*m, w - 2*m);
+                    else if ((blue & bit) != 0)
+                        e.Graphics.FillEllipse(Brushes.Blue, i * w + m, j * w + m, w - 2 * m, w - 2 * m);
+                    else if (hints && (board & bit) != 0)
+                        e.Graphics.FillRectangle(Brushes.Yellow, i * w + m, j * w + m, w - 2 * m, w - 2 * m);
+                }
+            }
+        }
+
+        void initTurn() {
+            if (blueTurn) {
+                initPoss(blue, red);
+                playerTurn.Text = "Blauw aan beurt";
+            } else {
+                initPoss(red, blue);
+                playerTurn.Text = "Rood aan beurt";
+            }
+        }
+
+        void initPoss(ulong P, ulong O) {
+            board = 0L;
+            for (byte i = 0; i < 64; i++) {
+                ulong bit = (ulong)1L << i;
+                if ((P & bit) != 0)
+                    board |= poss(P, O, i);
+            }
+        }
+
+        ulong poss_dir(ulong P, ulong O, byte move, int dX, int dY) {
+            ulong flips = 0;
+            int i = (move % 8) + dX;
+            int j = (move / 8) + dY;
+
+            while ((i >= 0) && (i < 8) && (j >= 0) && (j < 8)) {
+                ulong bit = (ulong)1L << (j * 8 + i);
+                if ((O & bit) != 0)
+                    flips |= bit;
+                else if (flips != 0 && (P & bit) == 0)
+                    return bit;
+                else
+                    return 0;
+                i += dX;
+                j += dY;
+            }
+            return 0;
+        }
+        ulong poss(ulong P, ulong O, byte move) {
+            return move == 64 ? 0L :
+                   poss_dir(P, O, move, -1, -1)
+                 | poss_dir(P, O, move, -1, 0)
+                 | poss_dir(P, O, move, -1, +1)
+                 | poss_dir(P, O, move, 0, -1)
+                 | poss_dir(P, O, move, 0, +1)
+                 | poss_dir(P, O, move, +1, -1)
+                 | poss_dir(P, O, move, +1, 0)
+                 | poss_dir(P, O, move, +1, +1);
+        }
+        private void panel1_MouseClick(object sender, MouseEventArgs e) {
+            int x = 8 * e.X / 480;
+            int y = 8 * e.Y / 480;
+            ulong bit = (ulong)1L << (y * 8 + x);
+            if ((board & bit) != 0) {
+                if (blueTurn) {
+                    bit |= flipColor(blue, red, x, y);
+                    blue |= bit;
+                    red &= ~bit;
+                } else {
+                    bit |= flipColor(red, blue, x, y);
+                    red |= bit;
+                    blue &= ~bit;
+                }
+                blueTurn = !blueTurn;
+                panel1.Invalidate();
+            }
+        }
+
+        // flips discs in one direction
+        ulong flip_dir(ulong P, ulong O, byte move, int dX, int dY) {
+            ulong flips = 0;
+            int i = (move % 8) + dX;
+            int j = (move / 8) + dY;
+
+            while ((i >= 0) && (i < 8) && (j >= 0) && (j < 8))
+            {
+                ulong bit = (ulong)1L << (j * 8 + i);
+                if ((O & bit) != 0)
+                    flips |= bit;
+                else if ((P & bit) != 0)
+                    return flips; 
+                else 
+                    return 0;
+                i += dX;
+                j += dY;
+            }
+            return 0;
+        }
+
+        ulong flip(ulong P, ulong O, byte move) {
+            return move == 64 ? 0L :
+                   flip_dir(P, O, move, -1, -1)
+                 | flip_dir(P, O, move, -1, 0)
+                 | flip_dir(P, O, move, -1, +1)
+                 | flip_dir(P, O, move, 0, -1)
+                 | flip_dir(P, O, move, 0, +1)
+                 | flip_dir(P, O, move, +1, -1)
+                 | flip_dir(P, O, move, +1, 0)
+                 | flip_dir(P, O, move, +1, +1);
+        }
+
+        
+
+        ulong flipColor(ulong P, ulong O, int x, int y) {
+            return flip(P, O, (byte)(y * 8 + x));
+        }
+
+        private void new_game_Click(object sender, EventArgs e) {
+            initBoard();
+            panel1.Invalidate();
+        }
+
+        private void help_button_Click(object sender, EventArgs e) {
+            hints = !hints;
+            panel1.Invalidate();
+        }
+
+        static int countSetBitsRec(ulong num) {
+            int nibble = 0;
+            if (0 == num)
+                return num_to_bits[0];
+
+            // Find last nibble 
+            nibble = (int)(num & 0xf);
+
+            return num_to_bits[nibble] + countSetBitsRec(num >> 4);
+        }
+
+        void drawRaster(Graphics g) {
+            for(int i = 0; i <= 8; i++) {
+                g.DrawLine(Pens.Black, 0, i * w, 8 * w, i * w);
+            }
+            for (int i = 0; i <= 8; i++) {
+                g.DrawLine(Pens.Black, i * w, 0, i * w, 8 * w);
+            }
+        }
+    }
+}
